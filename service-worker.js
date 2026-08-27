2de563afc06bf3a7fc4c438d44657cec047c0d32
obedguzman27 <obedguzman27@gmail.com>
2026-08-27 08:09:22 -0500

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@
 // Los datos NO se guardan aquí (eso vive en localStorage / futuro backend),
 // solo el "cascarón" de la app (HTML, íconos).
 
-const CACHE_NAME = 'kontaly-v10';
+const CACHE_NAME = 'kontaly-v12';
 const ARCHIVOS_CACHE = [
   './',
   './index.html',
