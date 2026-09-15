0859179b2c4f0c79c8f72b5ee21cf9d720fdd3cb
obedguzman27 <obedguzman27@gmail.com>
2026-09-15 18:33:00 -0500

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@
 // Los datos NO se guardan aquí (eso vive en localStorage / futuro backend),
 // solo el "cascarón" de la app (HTML, íconos).
 
-const CACHE_NAME = 'guzman-hvac-v44';
+const CACHE_NAME = 'guzman-hvac-v45';
 const ARCHIVOS_CACHE = [
   './',
   './index.html',
