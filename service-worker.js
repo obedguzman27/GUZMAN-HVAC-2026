66609e5df8267a12641f15c53e86ab4dd032e00f
GH MANAGER <obedguzman27@gmail.com>
2026-09-21 19:35:19 -0500

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@
 // Los datos NO se guardan aquí (eso vive en localStorage / futuro backend),
 // solo el "cascarón" de la app (HTML, íconos).
 
-const CACHE_NAME = 'gh-manager-v90';
+const CACHE_NAME = 'gh-manager-v92';
 const ARCHIVOS_CACHE = [
   './',
   './index.html',
